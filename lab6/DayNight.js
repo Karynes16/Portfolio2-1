fdfb88190fde3e70d470291f071352c8f68435bd
Karynes16 <97715666+Karynes16@users.noreply.github.com>
2022-04-29 23:38:13 -0400

    April 29, 2022 11:38 PM

--- lab6/DayNight.js
+++ lab6/DayNight.js
@@ -3,9 +3,9 @@ import { StyleSheet } from 'react-native';
 import { Button } from 'react-native';
 
 const Hello = () => {
-    const [night, updateNight] = useState(<h1>Good Night, its dark out!</h1>)
+    const [night, updateNight] = useState(<h1>Good Night everyone, its dark out!</h1>)
     const handleUpdate = () => {
-        updateNight (<h1>Good Morning, its Daylight now!</h1>)
+        updateNight (<h1>Good Morning everyone, its Daylight now!</h1>)
     }
     return (
         <div>
